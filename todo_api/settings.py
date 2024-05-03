5c98a96de7f46a02ac55da5fcd3e260950f9738e
SyedaShafi <syedashafi4@gmail.com>
2024-05-03 06:10:23 +0600

    urls added

--- todo_api/settings.py
+++ todo_api/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-# CSRF_TRUSTED_ORIGINS = ['live link domain',  'https://*.127.0.0.1' ]
+CSRF_TRUSTED_ORIGINS = ['https://drf-todo-app.onrender.com',  'https://*.127.0.0.1' ]
 
 # Application definition
 
